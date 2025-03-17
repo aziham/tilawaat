e401fb76482943fab29eaa93d7e8fde59918cf5d
aziham <hamza.aziane@pm.me>
2025-03-17 14:29:25 +0000

    Add PlaybackButton component

---
+++ src/components/Player/PlaybackButton.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@/components/ui/button';
+import { LucideIcon } from 'lucide-react';
+
+type PlaybackButtonProps = {
+  icon: LucideIcon;
+} & React.ComponentProps<'button'>;
+
+function PlaybackButton({ icon: Icon, ...props }: PlaybackButtonProps) {
+  return (
+    <Button
+      variant='ghost'
+      className="size-12 rounded-full [&_svg:not([class*='size-'])]:size-6"
+      {...props}
+    >
+      <Icon strokeWidth={1.3} />
+    </Button>
+  );
+}
+
+export default PlaybackButton;
