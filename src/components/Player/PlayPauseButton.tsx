10447b1b0d6ee88716548bbfc4d8773ee5ed3c7c
aziham <hamza.aziane@pm.me>
2025-03-17 14:28:16 +0000

    Add PlayPauseButton component

---
+++ src/components/Player/PlayPauseButton.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@/components/ui/button';
+import { Play, Pause } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+type PlayPauseButtonProps = {
+  isPlaying: boolean;
+  size: 'mini' | 'full';
+} & React.ComponentProps<'button'>;
+
+function PlayPauseButton({ isPlaying, size, ...props }: PlayPauseButtonProps) {
+  const Icon = isPlaying ? Pause : Play;
+
+  return (
+    <Button
+      className={cn(
+        'rounded-full',
+        size === 'mini'
+          ? 'size-10'
+          : "size-16 [&_svg:not([class*='size-'])]:size-7"
+      )}
+      {...props}
+    >
+      <Icon fill='white' />
+    </Button>
+  );
+}
+
+export default PlayPauseButton;
