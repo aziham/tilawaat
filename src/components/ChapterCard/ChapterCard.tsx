02112d9362de1b310c5495011894f63a2906057a
aziham <hamza.aziane@pm.me>
2025-03-23 08:51:46 +0000

    Update `ChapterCard` to use `RecitationProvider`
    - Added useRecitation to set the current recitation when a chapter is selected.
    - Replaced hardcoded values with dynamic `reciter` and `chapter` props.
    - Ensured correct TypeScript typings for `ChapterCardProps`.

--- src/components/ChapterCard/ChapterCard.tsx
+++ src/components/ChapterCard/ChapterCard.tsx
@@ -1,18 +1,38 @@
-import * as React from 'react';
-import { ChevronRightCircle } from 'lucide-react';
+'use client';
 
-function ChapterCard() {
+import { Reciter, Chapter } from '@/types';
+import { ChevronRightCircle } from 'lucide-react';
+import { useRecitation } from '@/contexts/RecitationProvider';
+
+type ChapterCardProps = {
+  reciter: Reciter;
+  chapter: Chapter;
+};
+
+function ChapterCard({ reciter, chapter }: ChapterCardProps) {
+  const { setRecitation } = useRecitation();
+
+  const handleClick = () =>
+    setRecitation({
+      reciterName: reciter.name,
+      chapterName: chapter.name,
+      reciterPhotoSrc: reciter.photoSrc,
+      audioSrc: chapter.audioSrc,
+      waveformSrc: chapter.waveformSrc
+    });
+
   return (
     <div
       role='button'
       className='flex justify-between items-center rounded-full  shadow-sm border cursor-pointer p-2'
+      onClick={handleClick}
     >
       <div className='flex items-center gap-3'>
         <div className='flex justify-center items-center font-semibold w-[50px] h-[50px] rounded-full bg-slate-200 text-slate-600'>
-          01
+          {chapter.id}
         </div>
         <div className='flex flex-col'>
-          <h2>Surat Al-Fatihah</h2>
+          <h2>Surat {chapter.name}</h2>
           <p className='text-muted-foreground text-xs'>Chapter</p>
         </div>
       </div>
