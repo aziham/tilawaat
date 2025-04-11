dba930763c52d8af58bcfd858310e8043cecbcc7
aziham <hamza.aziane@pm.me>
2025-04-11 12:30:00 +0000

    Feat: Add core fetcher for Tilawaat API

---
+++ api/fetch.ts
@@ -0,0 +1,7 @@
+const BASE_URL = `https://cdn.jsdelivr.net/gh/aziham/tilawaat-api`;
+
+export async function fetcher<T>(endpoint: string): Promise<T> {
+  const response = await fetch(`${BASE_URL}/${endpoint}`);
+  const data: T = await response.json();
+  return data;
+}
