90727f2946315a2762a9d716e3a50c5eb3129f27
aziham <hamza.aziane@pm.me>
2025-04-11 13:01:00 +0000

    Refactor: Migrate chapters API from local FS to remote fetcher

--- api/chapters.ts
+++ api/chapters.ts
@@ -1,13 +1,4 @@
-import fs from 'fs';
-import path from 'path';
 import type { Chapter } from './chapters.types';
+import { fetcher } from './fetch';
 
-const PATH = 'src/shared/data/chapters.json';
-
-export async function getChapters() {
-  const chaptersData: Chapter[] = JSON.parse(
-    fs.readFileSync(path.join(process.cwd(), PATH), 'utf8')
-  );
-
-  return chaptersData;
-}
+export const getChapters = () => fetcher<Chapter[]>('endpoint/chapters.json');
--- api/chapters.types.ts
+++ api/chapters.types.ts
@@ -1,4 +1,5 @@
 export interface Chapter {
-  id: string;
+  id: number;
   name: string;
+  type: number;
 }
