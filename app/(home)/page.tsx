7693179041c1c047f13264e0c7536c0b6347d90e
aziham <hamza.aziane@pm.me>
2025-04-21 12:00:00 +0100

    Add HomePage

---
+++ app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import { HeroSection } from '@/features/home/components/hero-section';
+import { RecitationsShelf } from '@/features/home/components/recitations-shelf';
+export default function HomePage() {
+  return (
+    <>
+      <HeroSection />
+      <RecitationsShelf />
+    </>
+  );
+}
