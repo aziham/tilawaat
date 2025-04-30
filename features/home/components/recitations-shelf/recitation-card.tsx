baae5182d8ddcd6d7c7992bcab1082d866245acb
aziham <hamza.aziane@pm.me>
2025-04-30 17:00:00 +0100

    Feat(recitations-shelf): Change cards size to 230px

--- features/home/components/recitations-shelf/recitation-card.tsx
+++ features/home/components/recitations-shelf/recitation-card.tsx
@@ -4,23 +4,17 @@ import { Play } from 'lucide-react';
 
 export function RecitationCard(recitation: FeaturedRecitation) {
   return (
-    <div className='hover:cursor-pointer'>
+    <div className='hover:cursor-pointer w-fit'>
       <div className='relative'>
         <Image
           src={recitation.reciter.photo_src}
           alt={`Reciter ${recitation.reciter.name} photo`}
-          width={300}
-          height={300}
+          width={230}
+          height={230}
           className='rounded-md'
         />
-        <div
-          className='
-          absolute top-0 left-0 
-          w-full h-[30%]
-          bg-gradient-to-b from-black/40 to-transparent rounded-md
-        '
-        />
-        <div className='bg-black/40 rounded-full w-fit p-3 absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2'>
+        <div className='absolute top-0 left-0 w-full h-[30%] bg-gradient-to-b from-black/50 to-transparent rounded-md' />
+        <div className='bg-black/40 rounded-full p-3 absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2'>
           <Play fill='white' color='white' />
         </div>
       </div>
