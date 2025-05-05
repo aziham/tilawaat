b7f27823024c55c2a098624808b29500a872791e
aziham <hamza.aziane@pm.me>
2025-05-05 16:00:00 +0100

    Feat(recitations-shelf): Add hover animation for play button on desktop while always visible on touch devices

--- features/home/components/recitations-shelf/recitation-card.tsx
+++ features/home/components/recitations-shelf/recitation-card.tsx
@@ -3,6 +3,10 @@ import type { FeaturedRecitation } from '@/api/recitations.types';
 import { Play } from 'lucide-react';
 
 export function RecitationCard(recitation: FeaturedRecitation) {
+  const canHover =
+    typeof window !== 'undefined' &&
+    window.matchMedia('(hover: hover)').matches;
+
   return (
     <div className='hover:cursor-pointer w-fit'>
       <div className='relative'>
@@ -14,7 +18,12 @@ export function RecitationCard(recitation: FeaturedRecitation) {
           className='rounded-md'
         />
         <div className='absolute top-0 left-0 w-full h-[30%] bg-gradient-to-b from-black/50 to-transparent rounded-md' />
-        <div className='bg-black/40 rounded-full p-3 absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2'>
+        <div
+          className={`bg-black/40 rounded-full p-3 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${
+            canHover &&
+            'opacity-0 -translate-y-1/3 group-hover:opacity-100 group-hover:-translate-y-1/2 transition-all duration-300 ease-in-out'
+          }`}
+        >
           <Play fill='white' color='white' />
         </div>
       </div>
