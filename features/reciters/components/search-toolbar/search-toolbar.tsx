aab8c18f65a9d1e799c6d3413e22e151943b2e8f
aziham <hamza.aziane@pm.me>
2025-05-22 18:00:00 +0100

    Refactor(reciters): Remove list/grid view buttons from SearchToolbar

--- features/reciters/components/search-toolbar/search-toolbar.tsx
+++ features/reciters/components/search-toolbar/search-toolbar.tsx
@@ -1,30 +1,19 @@
-import { Search, LayoutList, LayoutGrid } from 'lucide-react';
+import { Search } from 'lucide-react';
 import {
   InputGroup,
   InputGroupInput,
   InputGroupAddon
 } from '@/components/ui/input-group';
-import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export function SearchToolbar() {
   return (
-    <div className='mt-8 sm:mt-10 mx-5 flex items-center gap-3'>
+    <div className='flex items-center gap-3'>
       <InputGroup>
         <InputGroupInput placeholder='Search...' />
         <InputGroupAddon>
           <Search />
         </InputGroupAddon>
       </InputGroup>
-      <Tabs defaultValue='list'>
-        <TabsList className='h-10'>
-          <TabsTrigger value='list'>
-            <LayoutList /> List
-          </TabsTrigger>
-          <TabsTrigger value='grid'>
-            <LayoutGrid /> Grid
-          </TabsTrigger>
-        </TabsList>
-      </Tabs>
     </div>
   );
 }
